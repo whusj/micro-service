43e1d97f86753c2ce466e27a197d3a39fed3ff68
songjiang <whusj@163.com>
2018-10-02 13:39:12 +0800

    0.0.3

--- user-thrift-service/src/main/java/com/imooc/user/ServiceApplication.java
+++ user-thrift-service/src/main/java/com/imooc/user/ServiceApplication.java
@@ -1,7 +1,9 @@
 package com.imooc.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by Michael on 2017/10/28.
